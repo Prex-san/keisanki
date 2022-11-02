4ab37198987e2ef7aeed8a2c2ebb8432bdf6df29
Prex-san <100469780+Prex-san@users.noreply.github.com>
2022-11-03 02:54:23 +0530

    Add functionality with JavaScript
    A very simple program used to add functionalities to the calculator.

---
+++ scriptfile.js
@@ -0,0 +1,62 @@
+var a = 0;
+var b = 1;
+
+function pressOne(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 1;
+}
+function pressTwo(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 2;
+}
+function pressThree(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 3;
+}
+function pressFour(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 4;
+}
+function pressFive(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 5;
+}
+function pressSix(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 6;
+}
+function pressSeven(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 7;
+}
+function pressEight(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 8;
+}
+function pressNine(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 9;
+}
+function pressZero(){
+  prevVal = document.getElementById("display").value
+  document.getElementById("display").value = prevVal + 0;
+}
+function pressClear(){
+  document.getElementById("display").value = '';
+  a = 0;
+}
+function add(){
+  a = a+parseInt(document.getElementById("display").value);
+  document.getElementById("display").value = '';
+}
+function checkResult(){
+  document.getElementById("display").value = a;
+}
+function subtract(){
+  a = a-parseInt(document.getElementById("display").value);
+  document.getElementById("display").value = '';
+}
+function multiply(){
+  a = a*parseInt(document.getElementById("display").value);
+  document.getElementById("display").value = '';
+}
